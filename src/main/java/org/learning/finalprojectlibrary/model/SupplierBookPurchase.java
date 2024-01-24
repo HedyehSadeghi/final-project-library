99f38aaffbb909ecc1b8b798eb770cc8a6938295
hedys9 <hedysadeghi@gmail.com>
2024-01-24 12:50:30 +0100

    model SupplierBookPurchase

---
+++ src/main/java/org/learning/finalprojectlibrary/model/SupplierBookPurchase.java
@@ -0,0 +1,69 @@
+package org.learning.finalprojectlibrary.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "supplier_book_purchases")
+public class SupplierBookPurchase {
+    //ATTRIBUTI
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    @NotNull
+    private int amount;
+
+    @Column(nullable = false)
+    @DecimalMin(value = "1.0")
+    @NotNull
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    @PastOrPresent
+    @NotNull
+    private LocalDate date;
+
+
+    //GETTER E SETTER
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+
+}
