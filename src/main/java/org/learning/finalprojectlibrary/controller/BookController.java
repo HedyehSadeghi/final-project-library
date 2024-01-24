b355fb2f8ea34d087f8223d5e83f89528b90b85f
hedys9 <hedysadeghi@gmail.com>
2024-01-24 14:03:54 +0100

    fragment: resources book controller index and template

--- src/main/java/org/learning/finalprojectlibrary/controller/BookController.java
+++ src/main/java/org/learning/finalprojectlibrary/controller/BookController.java
@@ -1,4 +1,28 @@
 package org.learning.finalprojectlibrary.controller;
 
+import org.learning.finalprojectlibrary.model.Book;
+import org.learning.finalprojectlibrary.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/books")
 public class BookController {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @GetMapping
+    private String index(Model model) {
+        List<Book> bookList = bookRepository.findAll();
+        model.addAttribute("bookList", bookList);
+        return "books/list";
+    }
+
+
 }
