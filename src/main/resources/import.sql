213e3e1403c8ff9bf862c0efa147f4f1962d205f
Peppe2496 <giuseppemusellajob@gmail.com>
2024-01-24 16:10:25 +0100

    added categories in sql and categoryList page

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,10 @@
 INSERT INTO db_final_library.books(description, photo, price, title)VALUES('tale about a whale', 'https://m.media-amazon.com/images/I/71d5wo+-MuL._AC_UF1000,1000_QL80_.jpg', 10.50, 'Moby Dick');
 INSERT INTO db_final_library.books(description, photo, price, title)VALUES('tale about a wizard', 'https://m.media-amazon.com/images/I/911370CYBTL._AC_UF1000,1000_QL80_.jpg', 12.50, 'Harry Potter - Prisoner of Azkaban');
 INSERT INTO db_final_library.books(description, photo, price, title)VALUES('tale about a wizard', 'https://m.media-amazon.com/images/I/71sH3vxziLL._AC_UF1000,1000_QL80_.jpg', 13, 'Harry Potter - Deathly Hallows');
+
+INSERT INTO db_final_library.categories( name)VALUES('Bambini e Ragazzi');
+INSERT INTO db_final_library.categories( name)VALUES('Gialli e Thriller');
+INSERT INTO db_final_library.categories( name)VALUES('Narrativa Straniera');
+INSERT INTO db_final_library.categories( name)VALUES('Fantasia e Fantascienza');
+INSERT INTO db_final_library.categories( name)VALUES('Religione e Spiritualità');
+INSERT INTO db_final_library.categories( name)VALUES('Psicologia');
