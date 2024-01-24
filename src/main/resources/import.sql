00d708afe7217c764e5c3b794f869d02a2206c08
hedys9 <hedysadeghi@gmail.com>
2024-01-24 17:36:31 +0100

    fixed import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,10 +4,10 @@ INSERT INTO db_final_library.books(description, photo, price, title)VALUES('tale
 INSERT INTO db_final_library.books(description, photo, price, title)VALUES('tale about a wizard', 'https://m.media-amazon.com/images/I/71sH3vxziLL._AC_UF1000,1000_QL80_.jpg', 13, 'Harry Potter - Deathly Hallows');
 
 --import client purchases--
-INSERT INTO db_final_library.client_purchases( amount, `date`, price, book_id_id)VALUES( 5, '2024-01-24', 76.80,1 );
-INSERT INTO db_final_library.client_purchases( amount, `date`, price, book_id_id)VALUES( 15, '2024-01-24', 160.32,2 );
-INSERT INTO db_final_library.client_purchases( amount, `date`, price, book_id_id)VALUES( 2, '2024-01-24', 40.1,1 );
-INSERT INTO db_final_library.client_purchases( amount, `date`, price, book_id_id)VALUES( 31, '2024-01-24', 450.23,2);
+INSERT INTO db_final_library.client_purchases(amount, `date`, price, book_id_client_id)VALUES( 5, '2024-01-24', 76.80,1 );
+INSERT INTO db_final_library.client_purchases(amount, `date`, price, book_id_client_id)VALUES( 15, '2024-01-24', 160.32,2 );
+INSERT INTO db_final_library.client_purchases(amount, `date`, price, book_id_client_id)VALUES( 2, '2024-01-24', 40.1,1 );
+INSERT INTO db_final_library.client_purchases(amount, `date`, price, book_id_client_id)VALUES( 31, '2024-01-24', 450.23,2);
 
 
 --import categories--
