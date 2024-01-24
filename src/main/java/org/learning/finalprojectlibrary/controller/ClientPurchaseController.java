189c262918ffc5597d98608e51cb8785a355c56a
musella95 <musellamatteoo@hotmail.com>
2024-01-24 18:14:48 +0100

    fixed 3 template create.html

--- src/main/java/org/learning/finalprojectlibrary/controller/ClientPurchaseController.java
+++ src/main/java/org/learning/finalprojectlibrary/controller/ClientPurchaseController.java
@@ -1,12 +1,16 @@
 package org.learning.finalprojectlibrary.controller;
 
+import jakarta.validation.Valid;
 import org.learning.finalprojectlibrary.model.ClientPurchase;
 import org.learning.finalprojectlibrary.repository.BookRepository;
 import org.learning.finalprojectlibrary.repository.ClientPurchaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -33,4 +37,12 @@ public class ClientPurchaseController {
         return "clients/create";
     }
 
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("clientPurchase") ClientPurchase formClientPurchase, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "clients/create";
+        }
+        ClientPurchase savedClientPurchase = clientPurchaseRepository.save(formClientPurchase);
+        return "redirect:/clients/purchaseslist";
+    }
 }
