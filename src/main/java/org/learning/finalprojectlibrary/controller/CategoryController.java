c01dfd0f456c18a5a1af6268f358a5e5f1c5f6a0
Peppe2496 <giuseppemusellajob@gmail.com>
2024-01-24 15:44:02 +0100

    updated category controller

--- src/main/java/org/learning/finalprojectlibrary/controller/CategoryController.java
+++ src/main/java/org/learning/finalprojectlibrary/controller/CategoryController.java
@@ -1,7 +1,27 @@
 package org.learning.finalprojectlibrary.controller;
 
+import org.learning.finalprojectlibrary.model.Category;
+import org.learning.finalprojectlibrary.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/categories")
 public class CategoryController {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @GetMapping
+    private String index(Model model){
+        List<Category> categoryList = categoryRepository.findAll();
+        model.addAttribute("categoryList", categoryList);
+        return "categories/list";
+
+    }
 }
