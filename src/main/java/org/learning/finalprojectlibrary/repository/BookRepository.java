cf27cf1fb7bd98c0220caf81e70b10b44c8af634
Sasi2294 <montella.salvatore@live.it>
2024-01-24 12:49:52 +0100

    Esteso bookrepository

--- src/main/java/org/learning/finalprojectlibrary/repository/BookRepository.java
+++ src/main/java/org/learning/finalprojectlibrary/repository/BookRepository.java
@@ -1,4 +1,7 @@
 package org.learning.finalprojectlibrary.repository;
 
-public interface BookRepository {
+import org.learning.finalprojectlibrary.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Integer> {
 }
