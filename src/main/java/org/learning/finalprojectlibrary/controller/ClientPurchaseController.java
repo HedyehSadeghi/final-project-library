c1fa1059151fa4bcf20cc425a4b68dbc5ca2baf4
hedys9 <hedysadeghi@gmail.com>
2024-01-24 21:29:06 +0100

    one template in clientPurchases:form and crud

--- src/main/java/org/learning/finalprojectlibrary/controller/ClientPurchaseController.java
+++ src/main/java/org/learning/finalprojectlibrary/controller/ClientPurchaseController.java
@@ -5,15 +5,16 @@ import org.learning.finalprojectlibrary.model.ClientPurchase;
 import org.learning.finalprojectlibrary.repository.BookRepository;
 import org.learning.finalprojectlibrary.repository.ClientPurchaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/clients")
@@ -27,22 +28,60 @@ public class ClientPurchaseController {
     public String index(Model model) {
         List<ClientPurchase> clientPurchaseList = clientPurchaseRepository.findAll();
         model.addAttribute("clientPurchaseList", clientPurchaseList);
-        return "clients/purchaseslist";
+        return "clients/purchases-list";
     }
 
-    @GetMapping("/create")
+    @GetMapping("/create-purchase")
     public String create(Model model) {
         ClientPurchase clientPurchase = new ClientPurchase();
         model.addAttribute("clientPurchase", clientPurchase);
-        return "clients/create";
+        return "clients/purchase-form";
     }
 
-    @PostMapping("/create")
+    @PostMapping("/create-purchase")
     public String store(@Valid @ModelAttribute("clientPurchase") ClientPurchase formClientPurchase, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
-            return "clients/create";
+            return "clients/purchase-form";
         }
         ClientPurchase savedClientPurchase = clientPurchaseRepository.save(formClientPurchase);
         return "redirect:/clients/purchases";
     }
+
+
+    @GetMapping("/edit-purchase/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        Optional<ClientPurchase> result = clientPurchaseRepository.findById(id);
+        if (result.isPresent()) {
+            model.addAttribute("clientPurchase", result.get());
+            return "clients/purchase-form";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "purchase with id " + id + " not found");
+        }
+    }
+
+    @PostMapping("/edit-purchase/{id}")
+    public String update(@PathVariable Integer id, @Valid @ModelAttribute("clientPurchase") ClientPurchase clientPurchaseForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "clients/purchase-form";
+        }
+        clientPurchaseRepository.save(clientPurchaseForm);
+        return "redirect:/clients/purchases";
+
+    }
+
+    @PostMapping("/delete-purchase/{id}")
+    public String delete(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
+        Optional<ClientPurchase> result = clientPurchaseRepository.findById(id);
+        if (result.isPresent()) {
+            ClientPurchase clientPurchaseToDelete = result.get();
+            clientPurchaseRepository.delete(clientPurchaseToDelete);
+            redirectAttributes.addFlashAttribute("redirectMessage", "purchase with id  " + id + " deleted");
+            return "redirect:/clients/purchases";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "purchase with id " + id + " not found");
+        }
+
+    }
+
+
 }
