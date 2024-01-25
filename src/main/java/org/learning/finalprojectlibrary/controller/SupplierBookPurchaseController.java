d753f1ebcd86d442031d1fde655f327216c670be
Sasi2294 <montella.salvatore@live.it>
2024-01-25 12:50:44 +0100

    Curata la parte controller e template della lista di fornitori.

--- src/main/java/org/learning/finalprojectlibrary/controller/SupplierBookPurchaseController.java
+++ src/main/java/org/learning/finalprojectlibrary/controller/SupplierBookPurchaseController.java
@@ -1,25 +1,63 @@
 package org.learning.finalprojectlibrary.controller;
 
+import jakarta.validation.Valid;
+import org.learning.finalprojectlibrary.model.Category;
 import org.learning.finalprojectlibrary.model.SupplierBookPurchase;
+import org.learning.finalprojectlibrary.repository.BookRepository;
 import org.learning.finalprojectlibrary.repository.SupplierBookPurchaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/supplierpurchases")
+@RequestMapping("/suppliers")
 public class SupplierBookPurchaseController {
     @Autowired
     private SupplierBookPurchaseRepository supplierBookPurchaseRepository;
+    @Autowired
+    private BookRepository bookRepository;
 
-    @GetMapping
+    @GetMapping("/purchases")
     public String index(Model model) {
-        List<SupplierBookPurchase> supplierpurchases = supplierBookPurchaseRepository.findAll();
+        List<SupplierBookPurchase> supplierpurchases = supplierBookPurchaseRepository.findAll(Sort.by("date").descending());
         model.addAttribute("supplierPurchases", supplierpurchases);
         return "suppliers/purchases";
+    }
+
+    @PostMapping("/delete-purchase/{id}")
+    public String delete(@PathVariable Integer id) {
+        Optional<SupplierBookPurchase> result = supplierBookPurchaseRepository.findById(id);
+        if (result.isPresent()) {
+            supplierBookPurchaseRepository.deleteById(id);
+            return "redirect:/suppliers/purchases";
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Supplier purchase with id" + id + " not found");
+        }
+    }
+
+    @GetMapping("/create-purchase")
+    public String create(Model model) {
+        model.addAttribute("formSupp", new SupplierBookPurchase());
+        model.addAttribute("bookList", bookRepository.findAll());
+        return "suppliers/form";
+    }
+
+    @PostMapping("/create-purchase")
+    public String store(@Valid @ModelAttribute("formSupp") SupplierBookPurchase formSupp, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("bookList", bookRepository.findAll());
+            return "suppliers/form";
+        }
+        supplierBookPurchaseRepository.save(formSupp);
+        return "redirect:/suppliers/purchases";
     }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -31,3 +31,7 @@ INSERT INTO db_final_library.suppliers(name)VALUES('Mondadori');
 
 --import supplier-book-purchases --
 INSERT INTO db_final_library.supplier_book_purchases(amount, `date`, price, book_id, supplier_id)VALUES(50, '2023-12-20', 200.0, 2, 1);
+INSERT INTO db_final_library.supplier_book_purchases(amount, `date`, price, book_id, supplier_id)VALUES(50, '2023-12-10', 200.0, 1, 1);
+INSERT INTO db_final_library.supplier_book_purchases(amount, `date`, price, book_id, supplier_id)VALUES(50, '2023-12-13', 200.0, 1, 1);
+INSERT INTO db_final_library.supplier_book_purchases(amount, `date`, price, book_id, supplier_id)VALUES(50, '2023-12-11', 200.0, 2, 1);
+INSERT INTO db_final_library.supplier_book_purchases(amount, `date`, price, book_id, supplier_id)VALUES(50, '2023-12-29', 200.0, 2, 1);
