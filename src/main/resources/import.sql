929b817db331dc6cea796af95381f6efdb0253db
hedys9 <hedysadeghi@gmail.com>
2024-01-25 11:28:05 +0100

    more imports

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,12 +2,17 @@
 INSERT INTO db_final_library.books(description, photo, price, title)VALUES('tale about a whale', 'https://m.media-amazon.com/images/I/71d5wo+-MuL._AC_UF1000,1000_QL80_.jpg', 10.50, 'Moby Dick');
 INSERT INTO db_final_library.books(description, photo, price, title)VALUES('tale about a wizard', 'https://m.media-amazon.com/images/I/911370CYBTL._AC_UF1000,1000_QL80_.jpg', 12.50, 'Harry Potter - Prisoner of Azkaban');
 INSERT INTO db_final_library.books(description, photo, price, title)VALUES('tale about a wizard', 'https://m.media-amazon.com/images/I/71sH3vxziLL._AC_UF1000,1000_QL80_.jpg', 13, 'Harry Potter - Deathly Hallows');
+INSERT INTO db_final_library.books(description, photo, price, title)VALUES('tale about a wizard', 'https://upload.wikimedia.org/wikipedia/en/7/70/Harry_Potter_and_the_Order_of_the_Phoenix.jpg', 14, 'Harry Potter - Order of Phoenix');
+INSERT INTO db_final_library.books(description, photo, price, title)VALUES('tale about a wizard', 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b5/Harry_Potter_and_the_Half-Blood_Prince_cover.png/220px-Harry_Potter_and_the_Half-Blood_Prince_cover.png', 13, 'Harry Potter - Half-Blood Prince');
 
 --import client purchases--
 INSERT INTO db_final_library.client_purchases(amount, `date`, price, book_id)VALUES( 5, '2024-01-24', 76.80,1 );
 INSERT INTO db_final_library.client_purchases(amount, `date`, price, book_id)VALUES( 15, '2024-01-24', 160.32,2 );
 INSERT INTO db_final_library.client_purchases(amount, `date`, price, book_id)VALUES( 2, '2024-01-24', 40.1,1 );
 INSERT INTO db_final_library.client_purchases(amount, `date`, price, book_id)VALUES( 31, '2024-01-24', 450.23,2);
+INSERT INTO db_final_library.client_purchases(amount, `date`, price, book_id)VALUES( 14, '2024-01-20', 500,4);
+INSERT INTO db_final_library.client_purchases(amount, `date`, price, book_id)VALUES( 29, '2024-01-19', 260,5);
+INSERT INTO db_final_library.client_purchases(amount, `date`, price, book_id)VALUES( 39, '2024-01-18', 400,3);
 
 
 --import categories--
@@ -17,6 +22,7 @@ INSERT INTO db_final_library.categories( name)VALUES('Narrativa Straniera');
 INSERT INTO db_final_library.categories( name)VALUES('Fantasia e Fantascienza');
 INSERT INTO db_final_library.categories( name)VALUES('Religione e Spiritualità');
 INSERT INTO db_final_library.categories( name)VALUES('Psicologia');
+INSERT INTO db_final_library.categories( name)VALUES('Narrativa');
 
 
 --import categories-books--
@@ -27,6 +33,12 @@ INSERT INTO db_final_library.categories_book_list(category_list_id, book_list_id
 
 --import supplier--
 INSERT INTO db_final_library.suppliers(name)VALUES('Mondadori');
+INSERT INTO db_final_library.suppliers(name)VALUES('Feltrinelli');
+INSERT INTO db_final_library.suppliers(name)VALUES('Fabbri');
+INSERT INTO db_final_library.suppliers(name)VALUES('Penguin');
+INSERT INTO db_final_library.suppliers(name)VALUES('Giunti');
+INSERT INTO db_final_library.suppliers(name)VALUES('Einaudi');
+INSERT INTO db_final_library.suppliers(name)VALUES('Bompiani');
 
 
 --import supplier-book-purchases --
