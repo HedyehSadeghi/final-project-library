13b25c800e9f239b0c85c897d9f525e41d2047a1
Hedy Sadeghi <hedysadeghi@gmail.com>
2024-01-27 00:05:47 +0100

    closes #14 and closes #13

--- src/main/java/org/learning/finalprojectlibrary/controller/HomeController.java
+++ src/main/java/org/learning/finalprojectlibrary/controller/HomeController.java
@@ -1,19 +1,37 @@
 package org.learning.finalprojectlibrary.controller;
 
+import org.learning.finalprojectlibrary.model.Book;
+import org.learning.finalprojectlibrary.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
-    @GetMapping("home")
-    private String home() {
-        return "home/landing-page";
-    }
+
+    @Autowired
+    private BookRepository bookRepository;
+
 
     @GetMapping()
-    private String index() {
+    private String index(@RequestParam(name = "keyword", required = false) String searchKeyword, Model model) {
+        List<Book> bookList;
+
+        if (searchKeyword != null) {
+            bookList = bookRepository.findByTitleContaining(searchKeyword);
+        } else {
+            bookList = bookRepository.findAll(Sort.by("ClientPurchases").descending());
+        }
+        model.addAttribute("bookList", bookList);
+        model.addAttribute("preloadSearch", searchKeyword);
+
         return "home/landing-page";
     }
 }
--- src/main/java/org/learning/finalprojectlibrary/repository/BookRepository.java
+++ src/main/java/org/learning/finalprojectlibrary/repository/BookRepository.java
@@ -3,5 +3,8 @@ package org.learning.finalprojectlibrary.repository;
 import org.learning.finalprojectlibrary.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, Integer> {
+    List<Book> findByTitleContaining(String searchKeyword);
 }
