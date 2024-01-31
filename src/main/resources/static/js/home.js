d5255cc95218d4e957c215f29d60c72530e1cc18
Hedy Sadeghi <hedysadeghi@gmail.com>
2024-01-31 22:52:37 +0100

    closes #37

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,12 +1,12 @@
 // seleziono l'elemento paragrafo
+
 var titleText = document.getElementById("book-title-best-sellers");
 
 // definisco la funzione che sottolinea il testo
 function underlineText(event) {
     event.preventDefault();
+
     titleText.style.textDecoration = "underline";
-
-
 }
 
 // definisco la funzione che rimuove la sottolineatura
@@ -14,3 +14,33 @@ function removeUnderline(event) {
     event.preventDefault();
     titleText.style.textDecoration = "none";
 }
+
+
+var titleText1 = document.getElementsByClassName("title-best-sellers");
+
+function underlineText1(element, event) {
+  event.preventDefault();
+  element.style.textDecoration = "underline";
+}
+
+function removeUnderline1(element, event) {
+  event.preventDefault();
+  element.style.textDecoration = "none";
+}
+
+for (var i = 0; i < titleText1.length; i++) {
+  var element = titleText1[i];
+  // aggiungi qui il codice per aggiungere gli event listener a element
+  // usando le funzioni underlineText1 e removeUnderline1
+  element.addEventListener("mouseover", function(event) {
+    underlineText1(this, event);
+  });
+  element.addEventListener("mouseout", function(event) {
+    removeUnderline1(this, event);
+  });
+}
+
+
+
+
+
